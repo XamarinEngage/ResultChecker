f884c5b94722345c6747bfc2b19d2169d8f6b0f9
haybankz <sunsyde2010@gmail.com>
2018-09-11 19:16:03 +0100

    Changed from getting data from database to getting getting data from a REST API endpoint.

--- ResultChecker/ResultChecker/MainPage.xaml.cs
+++ ResultChecker/ResultChecker/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace ResultChecker
         private List<String> ExamYear = new List<string>();
         private List<String> ExamType = new List<string>();
 
-        private CandidateRepository database = App.CandidateRepository;
+        //private CandidateRepository database = App.CandidateRepository;
         public MainPage()
 		{
 			InitializeComponent();
@@ -40,35 +40,41 @@ namespace ResultChecker
 
         }
 
-        private  void SubmitButtonClicked(object sender, EventArgs e)
+        private async void SubmitButtonClicked(object sender, EventArgs e)
         {
+
+            Loader.IsRunning = true;
+            
             var examYear = ExamYearPicker.SelectedItem as string;
             var examType = ExamTypePicker.SelectedItem as string;
 
             var examNo = ExamNoEntry.Text;
-            if (!(String.IsNullOrEmpty(examYear) || string.IsNullOrEmpty(examType) || string.IsNullOrEmpty(examNo))) { 
-            
-                List<Candidate> cand = database.GetResult(examType, Int32.Parse(examYear), examNo);
-                if (cand.Count > 0)
+            if (!(String.IsNullOrEmpty(examYear) || string.IsNullOrEmpty(examType) || string.IsNullOrEmpty(examNo))) {
+
+                //List<Candidate> cand = database.GetResult(examType, Int32.Parse(examYear), examNo);
+                Candidate candidate = await App.Service.GetResult(examYear, examType, examNo);
+                if (candidate != null)
                 {
-                    var cands = cand[0];
+                    //var cands = cand[0];
 
                     var page = new ResultDisplay();
-                    page.BindingContext = cands;
+                    page.BindingContext = candidate;
 
-                    Navigation.PushAsync(page);
+                    await Navigation.PushAsync(page);
                     ExamTypePicker.SelectedItem = null;
                     ExamYearPicker.SelectedItem = null;
                     ExamNoEntry.Text = null;
+                    Loader.IsRunning = false;
                 }
                 else
                 {
-                    DisplayAlert("Info", $"Result not found for candidate: {examNo} in {examType} {examYear}", "Ok");
+                    Loader.IsRunning = false;
+                    await  DisplayAlert("Info", $"Result not found for candidate: {examNo} in {examType} {examYear}", "Ok");
                 }
             }
             else
             {
-                DisplayAlert("Error", $"Please complete all fields", "Ok");
+                await DisplayAlert("Error", $"Please complete all fields", "Ok");
             }
 
         }
---
+++ ResultChecker/ResultChecker/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultChecker
+{
+    public class Service
+    {
+
+        HttpClient  client;
+
+        public Service()
+        {
+            client = new HttpClient
+            {
+                BaseAddress = new Uri("http://resultchecking.herokuapp.com")
+            };
+        }
+
+        public async Task<Candidate> GetResult(string examYear, string examType,  string examNo)
+        {
+            var response = client.GetAsync($"/result/search?examYear={examYear}&examType={examType}&examNo={examNo}");
+            var responseData = await response.Result.Content.ReadAsStringAsync();
+            var Candidate = JsonConvert.DeserializeObject<Candidate>(responseData);
+            return Candidate;
+        }
+    }
+}
